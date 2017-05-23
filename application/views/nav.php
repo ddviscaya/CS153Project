6b576b02d9712d19115e2be06dd46479611dd0b7
chrstnnglc <catfelizardo@gmail.com>
2017-05-23 20:45:26 +0800

    Admin can now create users

--- application/views/nav.php
+++ application/views/nav.php
@@ -13,11 +13,21 @@
                 <li><a href="<?php echo base_url(); ?>">Home</a></li>
                 <?php if ($this->session->userdata('LoggedIn')){ ?>
 
-                <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">dropdown</a>
-                <ul class="dropdown-menu">
-                <li>  <a href="<?php echo base_url(); ?>index.php/users/view_all">View Users</a></li>
-                <li>  <a href="<?php echo base_url(); ?>index.php/users/logout">Logout</a></li>
+                  <li class="dropdown">
+                    <?php if ($user['user_type'] == 'admin') { ?>
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Options</a>
+                      <ul class="dropdown-menu">
+                        <li>  <a href="<?php echo base_url(); ?>index.php/users/view_all">View Users</a></li>
+                        <li>  <a href="<?php echo base_url(); ?>index.php/users/create_user">Create a User</a></li>
+                        <li>  <a href="<?php echo base_url(); ?>index.php/users/logout">Logout</a></li>
+                      </ul>
+                    <?php } else { ?>
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">dropdown</a>
+                      <ul class="dropdown-menu">
+                        <li>  <a href="<?php echo base_url(); ?>index.php/users/view_all">View Users</a></li>
+                        <li>  <a href="<?php echo base_url(); ?>index.php/users/logout">Logout</a></li>
+                      </ul>
+                    <?php } ?>
 
                 </ul>
               </li>
